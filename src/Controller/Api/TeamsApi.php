31f9d0990a41d511bf9f7947328ce3b5b2c30a3c
houlder <lazahoulder@gmail.com>
2023-05-22 08:36:44 +0300

    update team

--- src/Controller/Api/TeamsApi.php
+++ src/Controller/Api/TeamsApi.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Api;
 
+use App\DataHandler\InputHandler\TeamInputHandler;
+use App\Dto\IntputDTO\TeamInput;
 use App\Entity\Team;
 use App\Repository\TeamRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -9,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('api/team')]
 class TeamsApi extends BaseApiController
@@ -27,14 +30,42 @@ class TeamsApi extends BaseApiController
         return $this->json($this->repository->findAll());
     }
 
-    #[Route('/{id}', name: 'api_team_delete', methods: ['DELETE'])]
-    public function createTeam(Request $request)
+    #[Route('/', name: 'api_team_create', methods: ['POST'])]
+    public function createTeam(
+        Request $request,
+        TeamInputHandler $handler,
+        ValidatorInterface $validator
+    ): Response
     {
-        $this->repository->remove($team, true);
-        $response = new Response();
-        $response->setStatusCode(Response::HTTP_NO_CONTENT);
+        dd($request->getContent());
+        /** @var TeamInput $input */
+        $input = $this->serializer->deserialize($request->getContent(), TeamInput::class, 'json');
+        $errors = $validator->validate($input);
+        if (count($errors)) {
+            return $this->json($errors, Response::HTTP_BAD_REQUEST);
+        }
+        $team = $handler->handle($input);
 
-        return $response;
+        return $this->json($team, Response::HTTP_CREATED);
+    }
+
+    #[Route('/{id}', name: 'api_team_update', methods: ['PUT'])]
+    public function updateTeam(
+        Request $request,
+        Team $team,
+        TeamInputHandler $handler,
+        ValidatorInterface $validator,
+    ): Response
+    {
+        /** @var TeamInput $input */
+        $input = $this->serializer->deserialize($request->getContent(), TeamInput::class, 'json');
+        $errors = $validator->validate($input);
+        if (count($errors)) {
+            return $this->json($errors, Response::HTTP_BAD_REQUEST);
+        }
+        $team = $handler->handle($input, $team);
+
+        return $this->json($team, Response::HTTP_OK);
     }
 
     #[Route('/{id}', name: 'api_team_delete', methods: ['DELETE'])]
---
+++ src/DataHandler/InputHandler/TeamInputHandler.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataHandler\InputHandler;
+
+use App\Dto\IntputDTO\TeamInput;
+use App\Entity\Team;
+use App\Repository\TeamRepository;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class TeamInputHandler
+{
+    public function __construct(
+        private TeamRepository $repository,
+    )
+    {
+
+    }
+
+    public function handle(TeamInput $teamInput, ?Team $team = null): Team|ConstraintViolationListInterface
+    {
+        $team = $team ?? new Team();
+        $team
+            ->setName($teamInput->name)
+            ->setCountry($teamInput->country)
+            ->setAcountBalance($teamInput->acountBalance)
+        ;
+
+        $this->repository->save($team, true);
+
+        return $team;
+    }
+}
